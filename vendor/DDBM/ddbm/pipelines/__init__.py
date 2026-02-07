5ccbf227041c50ad1b78cbe0e55ca40dacca2d27
Bili-Sakura <bili_sakura@zju.edu.cn>
2026-02-07 13:47:33 +0800

    delete old vendor

--- vendor/DDBM/ddbm/pipelines/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright 2024 The DDBM Authors and The Hugging Face Team.
-# Licensed under the Apache License, Version 2.0 (the "License");
-
-from .ddbm_pipeline import DDBMPipeline, DDBMPipelineOutput
-
-__all__ = ["DDBMPipeline", "DDBMPipelineOutput"]
